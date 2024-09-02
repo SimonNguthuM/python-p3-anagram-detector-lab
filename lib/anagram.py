82086013b4a2519399dc09937f55b21bfd301a8f
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-09-02 17:42:47 +0300

    anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word=word
+
+    def match(self, anagram_options):
+        sorted_word = sorted(self.word)
+        matches = [anagram for anagram in anagram_options if sorted(anagram) == sorted_word]
+        return matches
